330f80218bb942f55487bab62e11aed7cfd219e2
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2020-03-13 21:03:41 +0300

    [utils]: add fbclid to clearUtmParams

--- utils/clearUtmParams.js
+++ utils/clearUtmParams.js
@@ -1,7 +1,23 @@
 export default () => {
   const { location, history } = window;
-  const isUtmsInQuery = location.search.indexOf('utm_') !== -1;
-  if (isUtmsInQuery && history.replaceState) {
-    history.replaceState({}, '', location.toString().replace(/(&|\?)utm([_a-z0-9=]+)/g, ''));
+  const params = ['utm', 'fbclid'];
+  let newUrl = location.toString();
+  let replace = false;
+
+  params.forEach(p => {
+    const isInQuery = location.search.indexOf(p) !== -1;
+    if (isInQuery) {
+      // https://regex101.com/r/lROOeI/2
+      const regex = new RegExp(`(&|/?)${p}(_\\w+)?=([_\\w0-9]+)`, 'g');
+      newUrl = newUrl
+        .replace(regex, '')
+        .replace(/\?&+/, '?')
+        .replace(/\?$/, '');
+      replace = true;
+    }
+  });
+
+  if (replace && history.replaceState) {
+    history.replaceState({}, '', newUrl);
   }
 };
