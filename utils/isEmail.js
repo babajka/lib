ce3eaf1877cf8160b271fe1419af614e6d363df7
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2020-12-19 23:38:31 +0300

    add isEmail

---
+++ utils/isEmail.js
@@ -0,0 +1,5 @@
+const EMAIL_REGEX = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/;
+
+const isEmail = email => !!email?.match(EMAIL_REGEX);
+
+export default isEmail;
