078ac081022be468361c00aff3e2bdf3dd93224a
Ulad Bohdan <vlbohdan@gmail.com>
2021-03-14 22:21:28 +0300

    parse yandex music playlist links

---
+++ utils/parseYMPlaylistUrl.js
@@ -0,0 +1,12 @@
+const YM_PLAYLIST_URL_REGEX = /(?:http|https)?:\/\/(?:www\.)?music\.yandex\.(?:com|ru|by)\/users\/([a-zA-Z\d]+)\/playlists\/(\d+)/;
+
+const parseUrl = url => {
+  const match = YM_PLAYLIST_URL_REGEX.exec(url);
+  if (!match) {
+    return null;
+  }
+  const [_, playlistUsername, playlistId] = match;
+  return { playlistUsername, playlistId } ;
+};
+
+export default parseUrl;
